2f45708fade0b10919e4de2235c8f086000a9620
Matteo Figus <matteofigus@gmail.com>
2019-07-08 14:45:43 +0100

    More cleanup

--- src/web-ui/src/utils/gateway.js
+++ src/web-ui/src/utils/gateway.js
@@ -4,48 +4,4 @@ export default {
   getTasks() {
     return request("/tasks");
   }
-
-  // addUser(params) {
-  //   const externalImageId = uuid();
-  //   return request("/faces/add", "post", {
-  //     externalImageId,
-  //     image: params.image
-  //   }).then(() =>
-  //     request("/people", "post", {
-  //       externalImageId,
-  //       memberName: params.fullName,
-  //       jobTitle: params.jobTitle
-  //     })
-  //   );
-  // },
-
-  // detectFaces(image) {
-  //   return request("/faces/detect", "post", { image });
-  // },
-
-  // getEngagement() {
-  //   const timeDetected = new Date().getTime() - 3600 * 90;
-  //   return request(`/engagement?timeDetected=${timeDetected}`);
-  // },
-
-  // getPeople() {
-  //   return request("/people");
-  // },
-
-  // postEngagement(detectedFace) {
-  //   const normalize = x => detectedFace.emotions[x] || 0;
-
-  //   return request(`/engagement`, "post", {
-  //     timeDetected: new Date().getTime(),
-  //     angry: normalize("ANGRY"),
-  //     calm: normalize("CALM"),
-  //     happy: normalize("HAPPY"),
-  //     sad: normalize("SAD"),
-  //     surprised: normalize("SURPRISED")
-  //   });
-  // },
-
-  // searchFaces(image) {
-  //   return request(`/faces/search`, "post", { image });
-  // }
 };
