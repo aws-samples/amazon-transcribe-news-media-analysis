d69d3b158aefca342753a935052f147e79bc05cc
Matteo Figus <matteofigus@gmail.com>
2019-07-09 13:49:59 +0100

    Small improvements

--- src/web-ui/src/utils/index.js
+++ src/web-ui/src/utils/index.js
@@ -1,8 +1,8 @@
 export const sortByKey = (array, key) =>
-  array.sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
+  array.concat().sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
 
-export const toObj = (arr, key) => {
-  const obj = {};
-  arr.forEach(el => (obj[el[key].trim()] = el));
-  return obj;
-};
+export const toObj = (arr, key) =>
+  arr.reduce((obj, el) => {
+    obj[el[key].trim()] = el;
+    return obj;
+  }, {});
