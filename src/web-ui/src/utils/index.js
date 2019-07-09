cd4c796aea9c820e89f72eef1e488ddfd6d669e0
Matteo Figus <matteofigus@gmail.com>
2019-07-09 14:08:33 +0100

    One liner

--- src/web-ui/src/utils/index.js
+++ src/web-ui/src/utils/index.js
@@ -2,7 +2,4 @@ export const sortByKey = (array, key) =>
   array.concat().sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
 
 export const toObj = (arr, key) =>
-  arr.reduce((obj, el) => {
-    obj[el[key].trim()] = el;
-    return obj;
-  }, {});
+  arr.reduce((obj, el) => ((obj[el[key].trim()] = el), obj), {});
