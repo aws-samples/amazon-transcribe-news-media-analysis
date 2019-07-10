9807936a75e24d401f52e93d6d60cb6322d2c712
Matteo Figus <matteofigus@gmail.com>
2019-07-11 00:51:27 +0100

    Video Player

--- src/frontend/src/utils/index.js
+++ src/frontend/src/utils/index.js
@@ -1,5 +1,17 @@
+export const getUrlParameter = name => {
+  name = name.replace(/[[]/, "\\[").replace(/[\]]/, "\\]");
+  const regex = new RegExp(`[\\?&]${name}=([^&#]*)`);
+  const results = regex.exec(window.location.search);
+
+  return results === null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
 export const sortByKey = (array, key) =>
-  array.concat().sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
+  array
+    .concat()
+    .sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
 
 export const toObj = (arr, key) =>
   arr.reduce((obj, el) => ((obj[el[key].trim()] = el), obj), {});
